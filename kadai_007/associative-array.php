885ca3edd5eb17e14c018bec682cf76c50e4c726
ensokei <ens0ke1@gmail.com>
2024-09-09 17:14:35 +0900

    kadai_007を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP基礎編_7章課題</title>
+    </head>
+    <body>
+        <p>
+            <?php
+            //値の代入
+            $item=['name'=>'onion','price'=>'200','weight'=>'160'];
+            
+            //出力
+            print_r($item);
+            ?>
+        </p>
+    </body>
+</html>
